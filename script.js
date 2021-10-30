716c21156772e586a5dd6139ceeb310fa5798dbf
Nikita Ermakov <n.ermakov@andersenlab.com>
2021-10-30 03:02:34 +0300

    add clear.php and fix script.js

--- script.js
+++ script.js
@@ -1,6 +1,60 @@
-"use strict";
+
 let x, y, r;
+
+
+function checkY() {
+    let y = document.getElementById("Y");
+    if (y.value.trim() === "") {
+        wrongFieldY.textContent = "Поле Y должно быть заполнено";
+        return false;
+    }
+    y.value = y.value.substring(0, 10).replace(',', '.');
+    if (!(y.value && !isNaN(y.value))) {
+        wrongFieldY.textContent = "Y должен быть числом!";
+        return false;
+    }
+    if (y.value <= -5 || y.value >=3) {
+        wrongFieldY.textContent = "Y должен принадлежать промежутку: (-5; 3)!";
+        return false;
+    }
+    return true;
+}
+
+function checkX() {
+    let xButtons = document.getElementsByName("X");
+    let counter = 0
+    xButtons.forEach(x => {
+        if (x.checked)
+            counter++
+    })
+    if (counter === 0) {
+        wrongFieldX.textContent = "Вы должны выбрать одно значение X";
+        return false
+    }
+    return true;
+}
+
+function checkR() {
+    let rButtons = document.getElementsByName("R"); 
+    let counter = 0;
+    rButtons.forEach(r => {
+        if (r.checked)
+            counter++
+    })
+    if (counter === 0) {
+        wrongFieldR.textContent = "Вы должны выбрать одно значение R";
+        return false
+    }
+    return true;
+}
+
 function submit () {
+    wrongFieldX.textContent = "Choose X";
+    wrongFieldY.textContent = "Choose Y";
+    wrongFieldR.textContent = "Choose R";
+
+    if (checkX() && checkY() && checkR()) {    
+
     x = document.querySelector('input[name="X"]:checked').value;
     y = document.getElementById("Y").value;
     r = document.querySelector('input[name="R"]:checked').value;
@@ -11,10 +65,12 @@ function submit () {
     xhr.open("GET", url);
     xhr.send();
     xhr.onload = function () {
-        if (xhr.status != 200)
-            alert("Error " + xhr.status + " " + xhr.statusText);
-        $(".main__table tr:gt(0)").remove();  
+        if (xhr.status != 200) {
+            alert("Error " + xhr.status + " " + xhr.statusText); 
+            return;
+        }
 
+        $(".main__table tr:gt(0)").remove(); 
         let result = JSON.parse(xhr.responseText);
         for (let i in result.response) {
             let nRow = '<tr>';
@@ -28,7 +84,33 @@ function submit () {
             $('.result_table').append(nRow);
         }
     };
+    }
 
 }
 
+function clear () {
+    let url = "clear.php";
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", url);
+    xhr.send();
+    $(".result_table tr:gt(0)").remove();
+}
+
+$("input:checkbox").on('click', function() {
+    var $box = $(this);
+    if ($box.is(":checked")) {
+      var group = "input:checkbox[name='" + $box.attr("name") + "']";
+      $(group).prop("checked", false);
+      $box.prop("checked", true);
+    } else {
+      $box.prop("checked", false);
+    }
+  });
+
+
 document.getElementById("submit").addEventListener("click", submit);
+document.getElementById("clear").addEventListener("click", clear);
+
+let wrongFieldX = document.getElementById("wrong_field_X");
+let wrongFieldY = document.getElementById("wrong_field_Y");
+let wrongFieldR = document.getElementById("wrong_field_R");
